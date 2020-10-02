d2f25e8b0a981bd04ea78e91696e47b2555cc9ee
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2020-10-02 12:55:24 +0200

    push settings

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.log('DB connection successful');
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on Port ${port}`);
 });
